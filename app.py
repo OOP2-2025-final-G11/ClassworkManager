ba19aeedc585453485c129a20607b5041e5d797f
Haruto-Koshimizu <ko.haru414@icloud.com>
2026-01-07 04:01:36 +0900

    完了、削除の実装

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+from models.todo import Todo
+from models.classwork import Classwork
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -14,7 +17,9 @@ for blueprint in blueprints:
 # ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # 課題の完了、削除のために、期限順に並べ替えた課題を取得
+    todos = Todo.select().order_by(Todo.deadline.asc())
+    return render_template('index.html', todos=todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- routes/todo.py
+++ routes/todo.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from models.todo import Todo 
 
 # Blueprint
 todo_bp = Blueprint('todo', __name__, url_prefix='/todo')
@@ -10,3 +11,20 @@ todo_bp = Blueprint('todo', __name__, url_prefix='/todo')
 @todo_bp.route('/add')
 def todo():
     return render_template('todo_add.html')
+
+
+
+# 課題を完了にする
+@todo_bp.route('/complete/<int:todo_id>', methods=['POST'])
+def complete(todo_id):
+    todo = Todo.get_by_id(todo_id)
+    todo.is_finished = True 
+    todo.save() 
+    return redirect(url_for('index'))
+
+# 課題を削除する
+@todo_bp.route('/delete/<int:todo_id>', methods=['POST'])
+def delete(todo_id):
+    todo = Todo.get_by_id(todo_id)
+    todo.delete_instance()
+    return redirect(url_for('index'))
