0a557939ce05c4437e12ca2096a64a90e6eca3cf
viciemo <yudai3958@icloud.com>
2026-01-08 14:35:08 +0900

    geminiAPIから帰ってきたデータをclassworkテーブルに保存するようにしました。

--- routes/timetable.py
+++ routes/timetable.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify
-import os
-from services.gemini import analyze_with_gemini
 import json
+from services.gemini import analyze_with_gemini
+from models.classwork import Classwork
+from models.db import db
 
 # Blueprint
 timetable_bp = Blueprint('timetable', __name__, url_prefix='/timetable')
@@ -35,6 +36,23 @@ def upload_image():
     result = analyze_with_gemini(file)
 
     print(json.dumps(result, ensure_ascii=False, indent=2))
+
+    classworks = result.get("classworks", [])
+
+    # ===== DB保存処理 =====
+    with db.atomic():
+        #既存の授業データを全削除
+        Classwork.delete().execute()
+
+        #Geminiから得た授業データを保存
+        for cw in classworks:
+            Classwork.create(
+                name=cw.get("name", ""),
+                teacher=cw.get("teacher", ""),
+                place=cw.get("place", ""),
+                day_of_work=cw.get("day_of_work"),
+                period=cw.get("period")
+            )
 
     # 必ず JSON を返す
     return jsonify(result)
