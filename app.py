54b89f436f2f307f9f820296ebf7231232930b3d
MONSTER0129 <haruku0129@gmail.com>
2026-01-08 14:46:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
-from routes.classwork_dashboard import get_timetable, DAYS, PERIODS
+from routes.classwork_dashboard import get_timetable, DAYS, PERIODS, DAY_LABELS
 
 app = Flask(__name__)
 
@@ -19,6 +19,7 @@ def index():
         'index.html',
         timetable=get_timetable(),
         days=DAYS,
+        day_labels=DAY_LABELS,
         periods=PERIODS,
         )
 
